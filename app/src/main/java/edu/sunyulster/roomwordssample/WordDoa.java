b8ac097eebc03796912dd6d21e7794d8e90c7a7e
Ayesha Ilyas <annie16950@gmail.com>
2022-11-22 18:58:04 -0500

    Add Word Entity and Data Access Object for inserting one Word, deleting all the words, and getting all the words

---
+++ app/src/main/java/edu/sunyulster/roomwordssample/WordDoa.java
@@ -0,0 +1,26 @@
+package edu.sunyulster.roomwordssample;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDoa {
+
+    @Insert
+    void insert(Word word);
+
+    // no convenience annotation for deleting multiple entries
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    // when methods annotated with @Query are called, they must be performed in a separate thread
+    // methods annotated by the convenience annotations are automatically executed in a their own threads
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+}
